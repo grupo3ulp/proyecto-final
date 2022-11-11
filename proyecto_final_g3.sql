c610f2804851403d081cb7c24db6632b8ff649b5
Luna Lopardo <112993603+iwentothewoods@users.noreply.github.com>
2022-11-11 17:09:35 -0300

    Base de datos + esquema

---
+++ proyecto_final_g3.sql
@@ -0,0 +1,159 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-11-2022 a las 21:00:48
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyecto_final_g3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comida`
+--
+
+CREATE TABLE `comida` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `detalles` varchar(200) DEFAULT NULL,
+  `calorias` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dieta`
+--
+
+CREATE TABLE `dieta` (
+  `id` int(11) NOT NULL,
+  `id_paciente` int(11) NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_fin` date NOT NULL,
+  `peso_inicial` double NOT NULL,
+  `peso_deseado` double NOT NULL,
+  `limite_calorico` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `itemdieta`
+--
+
+CREATE TABLE `itemdieta` (
+  `id` int(11) NOT NULL,
+  `id_dieta` int(11) NOT NULL,
+  `id_comida` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paciente`
+--
+
+CREATE TABLE `paciente` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `apellido` varchar(20) NOT NULL,
+  `domicilio` varchar(20) NOT NULL,
+  `dni` int(11) NOT NULL,
+  `telefono` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `comida`
+--
+ALTER TABLE `comida`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `dieta`
+--
+ALTER TABLE `dieta`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_paciente` (`id_paciente`);
+
+--
+-- Indices de la tabla `itemdieta`
+--
+ALTER TABLE `itemdieta`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_dieta` (`id_dieta`),
+  ADD KEY `id_comida` (`id_comida`);
+
+--
+-- Indices de la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `comida`
+--
+ALTER TABLE `comida`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `dieta`
+--
+ALTER TABLE `dieta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `itemdieta`
+--
+ALTER TABLE `itemdieta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `dieta`
+--
+ALTER TABLE `dieta`
+  ADD CONSTRAINT `dieta_ibfk_1` FOREIGN KEY (`id_paciente`) REFERENCES `paciente` (`id`);
+
+--
+-- Filtros para la tabla `itemdieta`
+--
+ALTER TABLE `itemdieta`
+  ADD CONSTRAINT `itemdieta_ibfk_1` FOREIGN KEY (`id_dieta`) REFERENCES `dieta` (`id`),
+  ADD CONSTRAINT `itemdieta_ibfk_2` FOREIGN KEY (`id_comida`) REFERENCES `comida` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
