9892e76ab531723540a74c386a4e874c360c0d32
Marco Silva <nelsonmarcosilva@gmail.com>
2022-11-14 10:32:26 -0300

    Agregado re carpeta recursos

--- Tp_FInal_Nutri/src/tp_final_nutri/Tp_FInal_Nutri.java
+++ Tp_FInal_Nutri/src/tp_final_nutri/Tp_FInal_Nutri.java
@@ -1,10 +1,13 @@
 
 package tp_final_nutri;
 
+import TPF.Control.Conexion;
+import java.sql.Connection;
+
 public class Tp_FInal_Nutri {
 
     public static void main(String[] args) {
-      
+        Connection conec= Conexion.getConexion(); 
     }
     
 }
